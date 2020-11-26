83732f441d1bc89b1ca56733241e549d87693af0
Stephen Corwin <stephenjcorwin@gmail.com>
2020-11-26 12:31:33 -0500

    Fixed formatting for unformatted code watcher

--- src/user/watchers.ts
+++ src/user/watchers.ts
@@ -1,26 +1,28 @@
 import { IWatcher } from 'definitions/IWatcher'
 
-const CODE_REGEX = /```\n/
-
 export const WATCHER_UNFORMATTED_CODE: IWatcher = {
   name: 'unformatted code',
   handler: (client) => async () => {
     client.on('message', (msg) => {
-      if (msg.author.bot || !CODE_REGEX.test(msg.content)) return
+      // escape early if codeblock already has an id.
+      if (msg.author.bot || !/```\n/.test(msg.content)) return
+      let result = ''
+      // eslint-disable-next-line prettier/prettier
+      result += '**TIP**: Discord supports syntax highlighting by decorating the first **\\`\\`\\`** of a code block with a language id (ts, js, jsx, css, html, bash).\n'
 
-      msg.channel.send([
-        '**TIP**: Discord supports syntax highlighting by decorating the first `` ``` `` of a code block with a language id (ts, js, jsx, css, html, bash).',
-        '```',
-        '`\u200b`\u200b`ts',
-        '// input',
-        'console.log(\'Hello World\')',
-        '`\u200b`\u200b`',
-        '```',
-        '```ts',
-        '// output',
-        'console.log(\'Hello World\')',
-        '```',
-      ].join('\n'))
+      // eslint-disable-next-line prettier/prettier
+      result += '\\`\\`\\`ts\n'
+      result += '// input\n'
+      result += `console.log('Hello World')\n`
+      // eslint-disable-next-line prettier/prettier
+      result += '\\`\\`\\`\n'
+
+      result += '```ts\n'
+      result += '// output\n'
+      result += `console.log('Hello World')\n`
+      result += '```\n'
+
+      msg.channel.send(result)
     })
   },
 }
