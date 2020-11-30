8755eee2d740802c2eeb8a8f89e49c34564a9ede
Stephen Corwin <stephenjcorwin@gmail.com>
2020-11-30 12:25:07 -0500

    Update watchers.ts
    Preferred early return. Shortened the example.

--- src/user/watchers.ts
+++ src/user/watchers.ts
@@ -10,29 +10,24 @@ export const WATCHER_UNFORMATTED_CODE: IWatcher = {
       const invalidCode = codeblocks?.some((codeblock) => /```\n((.|\n)*?)```/g.test(codeblock))
 
       // escape early if codeblock already has an id.
-      if (msg.author.bot || !invalidCode) return
-
-      if (codeTipCount % 3 === 0) {
-        let result = ''
-        // eslint-disable-next-line prettier/prettier
-        result += '**TIP**: Discord supports syntax highlighting by decorating the first **\\`\\`\\`** of a code block with a language id (ts, js, jsx, css, html, bash).\n'
-
-        // eslint-disable-next-line prettier/prettier
-        result += '\\`\\`\\`ts\n'
-        result += '// input\n'
-        result += `console.log('Hello World')\n`
-        // eslint-disable-next-line prettier/prettier
-        result += '\\`\\`\\`\n'
-
-        result += '```ts\n'
-        result += '// output\n'
-        result += `console.log('Hello World')\n`
-        result += '```\n'
-
-        msg.channel.send(result)
-      }
-
+      if (msg.author.bot || !invalidCode ) return
+      if (!!(codeTipCount % 3)) return
       codeTipCount++
+      let result = ''
+      // eslint-disable-next-line prettier/prettier
+      result += '**TIP**: Discord supports syntax highlighting. Decorate the first **\\`\\`\\`** of a code block with a language id (ts, js, jsx, css, html, bash).\n'
+
+      // eslint-disable-next-line prettier/prettier
+      result += '\\`\\`\\`ts\n'
+      result += `console.log('input', 'Hello World')\n`
+      // eslint-disable-next-line prettier/prettier
+      result += '\\`\\`\\`\n'
+
+      result += '```ts\n'
+      result += `console.log('output', 'Hello World')\n`
+      result += '```\n'
+
+      msg.channel.send(result)
     })
   },
 }
