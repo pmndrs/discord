8aac9fc30ff06edb9140ebc81bf4a09cf6d6c875
Cody Bennett <hi@codyb.co>
2020-11-30 11:28:14 -0600

    Fix formatting watcher false positives (#5)
    * Updated code formatting watcher to validate by group.
    
    * Update watchers.ts
    
    Preferred early return. Shortened the example.
    
    Co-authored-by: Stephen Corwin <stephenjcorwin@gmail.com>

--- src/user/watchers.ts
+++ src/user/watchers.ts
@@ -1,25 +1,30 @@
 import { IWatcher } from 'definitions/IWatcher'
 
+let codeTipCount = 0
+
 export const WATCHER_UNFORMATTED_CODE: IWatcher = {
   name: 'unformatted code',
   handler: (client) => async () => {
     client.on('message', (msg) => {
+      const codeblocks = msg.content.match(/```(.|\n)*?```/g)
+      const invalidCode = codeblocks?.some((codeblock) => /```\n((.|\n)*?)```/g.test(codeblock))
+
       // escape early if codeblock already has an id.
-      if (msg.author.bot || !/```\n/.test(msg.content)) return
+      if (msg.author.bot || !invalidCode ) return
+      if (!!(codeTipCount % 3)) return
+      codeTipCount++
       let result = ''
       // eslint-disable-next-line prettier/prettier
-      result += '**TIP**: Discord supports syntax highlighting by decorating the first **\\`\\`\\`** of a code block with a language id (ts, js, jsx, css, html, bash).\n'
+      result += '**TIP**: Discord supports syntax highlighting. Decorate the first **\\`\\`\\`** of a code block with a language id (ts, js, jsx, css, html, bash).\n'
 
       // eslint-disable-next-line prettier/prettier
       result += '\\`\\`\\`ts\n'
-      result += '// input\n'
-      result += `console.log('Hello World')\n`
+      result += `console.log('input', 'Hello World')\n`
       // eslint-disable-next-line prettier/prettier
       result += '\\`\\`\\`\n'
 
       result += '```ts\n'
-      result += '// output\n'
-      result += `console.log('Hello World')\n`
+      result += `console.log('output', 'Hello World')\n`
       result += '```\n'
 
       msg.channel.send(result)
